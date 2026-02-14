77bfffb6202c9815801bdbc5fddc424903da1fd4
suho227 <tomato.202502.woosh227@gmail.com>
2026-02-15 03:50:48 +0900

    vercel 설정에 따른수정

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -1,14 +1,15 @@
-
+// src/sanity/client.ts
 import { createClient } from "next-sanity";
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = createClient({
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-    apiVersion: "2024-01-01",
+    // 🌟 환경 변수가 우선이지만, 없을 경우 직접 ID를 사용하도록 설정하여 빌드 에러를 방지합니다.
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'o656ekmv',
+    dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+    apiVersion: '2024-01-01',
     useCdn: false,
 });
-// 🌟 사진 주소를 만들어주는 도구 추가
+
 const builder = imageUrlBuilder(client);
 export function urlFor(source: any) {
     return builder.image(source);
